22f9b9c84d915c9ea141ddadbd6cf698899e0bf0
Anton <tsikalo.tsikalo@mail.ru>
2023-01-09 14:34:32 +0300

    Для работы с разными типами устройств

--- main.py
+++ main.py
@@ -26,12 +26,13 @@ except:
 finally:
     sg.theme('DarkAmber')
 
-    layout = [  [sg.InputText(key='-link-', default_text = link), sg.Text('Ссылка')],
-                [sg.InputText(key='-carid-', default_text= carid_str), sg.Text('Буи (id через пробел)')],
-                [sg.InputText(key='-d_start-', default_text= d_start_str), sg.Text('Начало (гггг-мм-дд чч:мм:сс)')],
-                [sg.InputText(key='-d_stop-', default_text= d_finish_str), sg.Text('Конец (гггг-мм-дд чч:мм:сс)')],
-                [sg.ProgressBar(max_value=100000, orientation='h', size=(30,15), key='-PROG-'), sg.Text('Обработка данных')],
-                [sg.Button('Старт'), sg.Button('Выход')]  ]
+    layout = [[sg.InputText(key='-link-', default_text=link), sg.Text('Ссылка')],
+              [sg.InputText(key='-carid-', default_text=carid_str), sg.Text('Буи (id через пробел)')],
+              [sg.InputText(key='-d_start-', default_text=d_start_str), sg.Text('Начало (гггг-мм-дд чч:мм:сс)')],
+              [sg.InputText(key='-d_stop-', default_text=d_finish_str), sg.Text('Конец (гггг-мм-дд чч:мм:сс)')],
+              [sg.ProgressBar(max_value=100000, orientation='h', size=(30, 15), key='-PROG-'),
+               sg.Text('Обработка данных')],
+              [sg.Button('Старт'), sg.Button('Выход')]]
 
     window = sg.Window('Ввод данных', layout, finalize=True)
 
@@ -47,7 +48,9 @@ finally:
             d_start = pd.to_datetime(values['-d_start-'])
             d_finish = pd.to_datetime(values['-d_stop-'])
 
-            if ((link == '') or (carid_str == '') or (d_start_str == '') or (d_finish_str == '')) and ((values['-link-'] == '') or (values['-carid-'] == '') or (values['-d_start-'] == '') or (values['-d_stop-'] == '')):
+            if ((link == '') or (carid_str == '') or (d_start_str == '') or (d_finish_str == '')) and (
+                    (values['-link-'] == '') or (values['-carid-'] == '') or (values['-d_start-'] == '') or (
+                    values['-d_stop-'] == '')):
                 sg.popup('проверьте ini')
                 break
 
@@ -74,27 +77,30 @@ finally:
         window['-PROG-'].update_bar(20000)
         data_all_code = soup.find_all('code')
         window['-PROG-'].update_bar(35000)
-        data_col_names = data_all_code[20].text.split('&')
-        columns_names = [data.split('=')[0] for data in data_col_names]
+        columns_names = ['carid', 'lat', 'lon', 'sats', 'HDOP', 'platform_type', 'count', 't_surf', 't_sub_surf',
+                            'turbidity', 'turbidity_mg',  'hp', 'submergence', 'rough', 'level_plc', 'pwr_v_main',
+                            'mdate', 'mtime', 'turb_debug', 'frame', 'test']
+        columns_names_rb = []
+        columns_names_pb = []
+        columns_names_ws = []
 
         data_all = []
-        step = 65000/len(data_all_code)
+        step = 65000 / len(data_all_code)
 
         for i, elem in enumerate(data_all_code):
             elem = elem.text.split('&')
             data_all.append([data.split('=')[1] for data in elem])
-            window['-PROG-'].update_bar(35000 + int(i*step))
-
+            window['-PROG-'].update_bar(35000 + int(i * step))
 
         index = [x for x in range(1, len(data_all) + 1)]
         result_data = pd.DataFrame(data_all, index, columns_names)
 
-        result_data['lat'] = result_data['lat'].astype('float')
-        result_data['lon'] = result_data['lon'].astype('float')
-        result_data['HDOP'] = result_data['HDOP'].astype('float')
-        result_data['t_surf'] = result_data['t_surf'].astype('float')
-        result_data['t_sub_surf'] = result_data['t_sub_surf'].astype('float')
-        result_data['pwr_v_main'] = result_data['pwr_v_main'].astype('float')
+        if 'lat' in result_data.columns: result_data['lat'] = result_data['lat'].astype('float')
+        if 'lon' in result_data.columns: result_data['lon'] = result_data['lon'].astype('float')
+        if 'HDOP' in result_data.columns: result_data['HDOP'] = result_data['HDOP'].astype('float')
+        if 't_surf' in result_data.columns: result_data['t_surf'] = result_data['t_surf'].astype('float')
+        if 't_sub_surf' in result_data.columns: result_data['t_sub_surf'] = result_data['t_sub_surf'].astype('float')
+        if 'pwr_v_main' in result_data.columns: result_data['pwr_v_main'] = result_data['pwr_v_main'].astype('float')
 
         result_data['mdate'] = result_data['mdate'] + result_data['mtime']
         result_data.rename(columns={'mdate': 'mdate_time'}, inplace=True)
@@ -107,9 +113,5 @@ finally:
 
         for carid in carid_list:
             xlsx_maker.xlsx_maker(carid, result_data, cur_dir, d_start, d_finish)
-
+        sg.popup('Обработка завершена')
     window.close()
-
-
-
-
